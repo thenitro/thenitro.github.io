f4469eca521d08ffaafeb2a0676cf4644c671a2f
Timur Koshel <timurkoshel@gmail.com>
2020-01-29 22:04:31 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+ $('.slider1').bxSlider({
+            mode: 'fade',
+            captions: false,
+            auto: true,
+            pager: false,
+
+        });
+        $('.slider2').bxSlider({
+            pager: false,
+            captions: true,
+            maxSlides: 3,
+            minSlides: 1,
+            slideWidth: 230,
+            slideMargin: 10
+        });
+        $('.slider3').bxSlider({
+            mode: 'fade',
+            captions: false,
+            auto: true,
+            pager: false,
+            controls: false,
+        });
